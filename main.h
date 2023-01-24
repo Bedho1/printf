12088adb4f4b9a267c8aa1815a76deba7b125507
ChalaLe <clelisa28@gmail.com>
2023-01-24 11:26:03 -0800

    team work

--- main.h
+++ main.h
@@ -1,39 +1,78 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdarg.h>
-#include <stdio.h>
-#include <unistd.h>
-#define UNUSED(x) (void)(x)
-#define BUFF_SIZE 1024
-#define F_MINUS 1
-#define F_PLUS 2
-#define F_ZERO 4
-#define F_HASH 8
-#define F_SPACE 16
-/* SIZES */
-#define S_LONG 2
-#define S_SHORT 1
 
-int _printf(const char *format, ...);
+#include <stdlib.h>
+#include <stdarg.h>
+
+/**
+ * struct flag - struct containing flags to "turn on"
+ * when a flag specifier is passed to _printf()
+ * @plus: flag for the '+' character
+ * @space: flag for the ' ' character
+ * @hash: flag for the '#' character
+ */
+typedef struct flag
+{
+	int plus;
+	int space;
+	int hash;
+} flag_t;
+
+/**
+ * struct printHandler - struct to choose the right function depending
+ * on the format specifier passed to _printf()
+ * @c: format specifier
+ * @f: pointer to the correct printing function
+ */
+typedef struct printHandler
+{
+	char c;
+	int (*f)(va_list ap, flags_t *f);
+} ph;
+
+
 int print_int(va_list l, flags_t *f);
-int print_unsigned(va_list l, flags_t *f);
 void print_number(int n);
+int print_unsigned(va_list l, flags_t *f);
 int count_digit(int i);
+
+
 int print_hex(va_list l, flags_t *f);
 int print_hex_big(va_list l, flags_t *f);
 int print_binary(va_list l, flags_t *f);
 int print_octal(va_list l, flags_t *f);
+
+
+char *convert(unsigned long int num, int base, int lowercase);
+
+int _printf(const char *format, ...);
+
+int (*get_print(char s))(va_list, flags_t *);
+
+int get_flag(char s, flags_t *f);
+int get_flag(const char *format, int *i)
+
+int print_string(va_list l, flags_t *f);
+int print_char(va_list l, flags_t *f);
+
+
 int _putchar(char c);
 int _puts(char *str);
-print_non_printable(va_list l, flags_t *f);
-int print_rev(va_list l, flags_t *f);
+
 int print_rot13(va_list l, flags_t *f);
-int print_percent(va_list l, flags_t *f);
+int print_rev(va_list l, flags_t *f);
+int print_print_non_printable(va_list l, flags_t *f);
+
+
 int print_address(va_list l, flags_t *f);
-int get_flag(const char *format, int *i);
-int print_size(const char *format, int *i);
-int print_width(const char *format, int *i, va_list list);
-int print_precision(const char *format, int *i, va_list list);
-int handle_print(const char *fmt, int *ind, va_list list, char buffer[], int flags, int width, int precision, int size);
+int print_size(const char *format, int *i)
+int print_width(const char *format, int *i, va_list list)
+int print_precision(const char *format, int *i, va_list list)
+
+int print_percent(va_list l, flags_t *f);
+
+int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
+	int flags, int width, int precision, int size)
+
 
 #endif
